5b9b45cfb67979c453c4a6446a422515e60a8312
Rollie Ma <rollie.ma@gmail.com>
2014-11-20 23:48:39 -0800

    Added node-quickbooks as a dependency

--- export.js
+++ export.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+var colors = require("colors");
+var quickbooks = require("node-quickbooks");
+
+var consumerKey = "";
+var consumerSecret = "";
+var oauthToken = "";
+var oauthTokenSecret = "";
+var realmId = "";
+var qbo = new QuickBooks(
+    consumerKey, consumerSecret, oauthToken, oauthTokenSecret, realmId, true);
