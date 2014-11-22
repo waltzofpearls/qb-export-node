4ddb7aa526eb26fc930e039304d484c9455a5f38
Rollie Ma <rollie.ma@gmail.com>
2014-11-21 23:42:07 -0800

    Added js2xmlparse for json conversion and use fs to write xml to file

--- export.js
+++ export.js
@@ -1,34 +1,47 @@
 #!/usr/bin/env node
 
+var file = require("fs");
 var colors = require("colors");
 var quickbooks = require("node-quickbooks");
+var js2xml = require("js2xmlparser");
 
 var consumerKey = "qyprdrXuXEN8yeabLlxlNqaXTdTSjC";
 var consumerSecret = "jv0OHMHtLjYF0c9Sr8tqUyzynBfEO8XWTAgPQUCH";
 var oauthToken = "qyprdor3EJyXgillJvwVkI5tFIcXTDodWiCD650AjlwUS1Wy";
 var oauthTokenSecret = "4iUq4Q6DGX3zkxeRvSAW8M7YeXDORc9xYWfZDqA1";
 var realmId = "386011406";
+var debug = false;
+
+var accountsFile = "./exports/accounts.xml";
+var vendorsFile = "./exports/vendors.xml";
+
+var xml;
 
 var qbo = new quickbooks(
-    consumerKey, consumerSecret, oauthToken, oauthTokenSecret, realmId, true);
+        consumerKey, consumerSecret, oauthToken, oauthTokenSecret, realmId, debug);
 
-qbo.findAccounts({
-    // AccountType: "Expense",
-    // desc: "MetaData.LastUpdatedTime",
-    // limit: 5,
-    // offset: 5
-}, function(_, accounts) {
-    accounts.QueryResponse.Account.forEach(function(account) {
-        console.log(account.Name);
+console.log("- Fetching accounts from QuickBooks API...");
+qbo.findAccounts({}, function(_, accounts) {
+    delete accounts.QueryResponse.startPosition;
+    delete accounts.QueryResponse.maxResults;
+
+    xml = js2xml("Accounts", accounts.QueryResponse);
+
+    file.writeFile(accountsFile, xml, function(error) {
+        console.log("- " + accountsFile + " " + "[DONE]".green);
+        xml = null;
     });
 });
 
-qbo.findVendors({
-    // desc: "MetaData.LastUpdatedTime",
-    // limit: 5,
-    // offset: 5
-}, function(_, vendors) {
-    vendors.QueryResponse.Vendor.forEach(function(vendor) {
-        console.log(vendor.CompanyName);
+console.log("- Fetching vendors from QuickBooks API...");
+qbo.findVendors({}, function(_, vendors) {
+    delete vendors.QueryResponse.startPosition;
+    delete vendors.QueryResponse.maxResults;
+
+    xml = js2xml("Vendors", vendors.QueryResponse);
+
+    file.writeFile(vendorsFile, xml, function(error) {
+        console.log("- " + vendorsFile + " " + "[DONE]".green);
+        xml = null;
     });
 });
