eab276343930bff2ccb87c7db782e76ea74cbb23
Rollie Ma <rollie.ma@gmail.com>
2014-11-22 13:42:00 -0800

    Changed export.js to use qbexport module

--- export.js
+++ export.js
@@ -1,48 +1,24 @@
 #!/usr/bin/env node
 
-var q = require("q");
-var fs = require("fs");
-var colors = require("colors");
-var quickbooks = require("node-quickbooks");
-var js2xmlparser = require("js2xmlparser");
+var qbexport = require("./libs/qbexport");
+var settings = require("./settings");
 
-var consumerKey = "qyprdrXuXEN8yeabLlxlNqaXTdTSjC";
-var consumerSecret = "jv0OHMHtLjYF0c9Sr8tqUyzynBfEO8XWTAgPQUCH";
-var oauthToken = "qyprdor3EJyXgillJvwVkI5tFIcXTDodWiCD650AjlwUS1Wy";
-var oauthTokenSecret = "4iUq4Q6DGX3zkxeRvSAW8M7YeXDORc9xYWfZDqA1";
-var realmId = "386011406";
-var debug = false;
+try {
+    var qe = new qbexport(settings);
 
-var accountsFile = "./exports/accounts.xml";
-var vendorsFile = "./exports/vendors.xml";
-
-var xml;
-
-var qbo = new quickbooks(
-        consumerKey, consumerSecret, oauthToken, oauthTokenSecret, realmId, debug);
-
-console.log("- Fetching accounts from QuickBooks API...");
-qbo.findAccounts({}, function(_, accounts) {
-    delete accounts.QueryResponse.startPosition;
-    delete accounts.QueryResponse.maxResults;
-
-    xml = js2xmlparser("Accounts", accounts.QueryResponse);
-
-    fs.writeFile(accountsFile, xml, function(error) {
-        console.log("- " + accountsFile + " " + "[DONE]".green);
-        xml = null;
-    });
-});
-
-console.log("- Fetching vendors from QuickBooks API...");
-qbo.findVendors({}, function(_, vendors) {
-    delete vendors.QueryResponse.startPosition;
-    delete vendors.QueryResponse.maxResults;
-
-    xml = js2xmlparser("Vendors", vendors.QueryResponse);
-
-    fs.writeFile(vendorsFile, xml, function(error) {
-        console.log("- " + vendorsFile + " " + "[DONE]".green);
-        xml = null;
-    });
-});
+    qe.fetchAccounts()
+        .then(function(response) {
+            return qe.writeAccounts(response);
+        })
+        .then(function() {
+            return qe.fetchVendors();
+        })
+        .then(function(response) {
+            return qe.writeVendors(response);
+        })
+        .fail(function(error) {
+            throw new Error(error);
+        });
+} catch (err) {
+    console.error(err);
+}
